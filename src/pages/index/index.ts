a3e9002efb8ce3d406ecd1841873ae930e7935c2
Reynier R <raalarcon9705@gmail.com>
2021-10-09 11:23:26 -0400

    main section

--- src/pages/index/index.ts
+++ src/pages/index/index.ts
@@ -1,5 +1,16 @@
-import { createParticles } from '../../features/particles/index';
 import { splitLetters } from '../../utils/dom';
 import './index.scss';
-
 splitLetters();
+
+const initView = () => {
+  const btnContactToMe = document.getElementById('btnContactToMe');
+  const sectionContactToMe = document.getElementById('contactToMe');
+
+  btnContactToMe.addEventListener('click', () => {
+    sectionContactToMe.scrollIntoView({ behavior: 'smooth' });
+  });
+};
+
+window.addEventListener('DOMContentLoaded', (event) => {
+  initView();
+});
